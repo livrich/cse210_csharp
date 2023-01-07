d0e0b174a620df39bd2fc81d41dfe50f4a5149b2
livrich <91569440+livrich@users.noreply.github.com>
2023-01-07 13:55:30 -0700

    Core requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercent = Console.ReadLine();
+        float percent = Single.Parse(gradePercent);
+
+        string letter = "F";
+        
+        do
+        {
+            if (percent >= 60)
+            {
+                if (percent >= 70)
+                {
+                    if (percent >= 80)
+                    {
+                        if (percent >= 90)
+                        {
+                            letter = "A";
+                            break;
+                        }
+                        letter = "B";
+                        break;
+                    }
+                    letter = "C";
+                    break;
+                }
+                letter = "D";
+            }
+        } while (false);
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed... :(");
+        }
+        
+
     }
 }
