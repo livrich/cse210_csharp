b55d0d6dced074d2ca60ab6ec26a62db6c0b83d8
livrich <91569440+livrich@users.noreply.github.com>
2023-01-07 14:29:20 -0700

    Added comments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,15 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Get grade percentage from user
         Console.Write("What is your grade percentage? ");
-        string gradePercent = Console.ReadLine();
-        double doublePercent = Math.Round(Single.Parse(gradePercent));
+        string answer = Console.ReadLine();
+        // Convert input from string to double
+        // Round input to whole number (must be double to round)
+        double doublePercent = Math.Round(Single.Parse(answer));
+        // Convert double to int to save memory space
         int percent = Convert.ToInt32(doublePercent);
 
+        // Default letter grade is F
         string letter = "F";
         
+        // Loop so we can break out of if statements
         do
         {
+            // Check letter grade in increments of 10
+
+            // if true, check next if statement.
+            // if next if statement is false, set 'letter'
+            // to current grade letter and break loop.
             if (percent >= 60)
             {
                 if (percent >= 70)
@@ -34,20 +45,27 @@ class Program
             }
         } while (false);
 
+        // Divide percent by 10 to get remainder
         int remainder = percent % 10;
+        // Default sign is nothing
         string sign = "";
 
+        // Check if letter grade should have a '+'
         if (remainder >= 7 && letter != "A" && letter != "F")
         {
             sign = "+";
         }
+        // Check if letter grade should have a '-'
         else if (remainder < 3 && letter != "F")
         {
             sign = "-";
         }
 
+        // Display letter grade to terminal
         Console.WriteLine($"Your letter grade is: {letter}{sign}");
 
+        // Determine if grade is enough to pass or fail
+        // Display appropriate message to terminal
         if (percent >= 70)
         {
             Console.WriteLine("You passed!");
@@ -56,7 +74,5 @@ class Program
         {
             Console.WriteLine("You failed... :(");
         }
-        
-
     }
 }
