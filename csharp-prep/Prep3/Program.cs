dc0723629c5a02806fec3f9daedc7099b82684af
livrich <91569440+livrich@users.noreply.github.com>
2023-01-14 13:40:40 -0700

    Stretch Challenges

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,34 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Console.Write("What is the magic number? ");
-        // string answerString = Console.ReadLine();
-        // int answer = Int32.Parse(answerString);
-
-        Random randomGenerator = new Random();
-        int answer = randomGenerator.Next(1, 101);
-
-        bool state = false;
-        while (state == false)
+        string playGame = "Y";
+        while (playGame == "Y")
         {
-            Console.Write("Enter guess: ");
-            string guessString = Console.ReadLine();
-            int guess = Int32.Parse(guessString);
+            // Console.Write("What is the magic number? ");
+            // string answerString = Console.ReadLine();
+            // int answer = Int32.Parse(answerString);
 
-            if (guess == answer)
+            Random randomGenerator = new Random();
+            int answer = randomGenerator.Next(1, 101);
+
+            int numGuesses = 0;
+            
+            bool state = false;
+            while (state == false)
             {
-                Console.WriteLine("You guessed the magic number!");
-                state = true;
+                Console.Write("Enter guess: ");
+                string guessString = Console.ReadLine();
+                int guess = Int32.Parse(guessString);
+                numGuesses += 1;
+
+                if (guess == answer)
+                {
+                    Console.WriteLine("You guessed the magic number!");
+                    state = true;
+                }
+                else if (guess > answer)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Higher");
+                }
             }
-            else if (guess > answer)
+
+            Console.WriteLine($"It took you {numGuesses} guesses");
+
+            bool valid = false;
+            while (valid == false)
             {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("Higher");
+                Console.Write("Would you like to play again? Y/N: ");
+                string again = Console.ReadLine();
+                string playAgain = again.ToUpper();
+
+                if (playAgain == "N")
+                {
+                    Console.WriteLine("Thanks for playing, bye!");
+                    playGame = "N";
+                    valid = true;
+                }
+                else if (playAgain == "Y")
+                {
+                    valid = true;
+                }
+                else
+                {
+                    Console.WriteLine("That was not an option. Please pick again.");
+                }
             }
         }
-        
-    }
+    }    
 }
