4ef9e1787fcb33bd894315b5027eca83f0a3975a
livrich <91569440+livrich@users.noreply.github.com>
2023-01-14 15:48:44 -0700

    Added code and comments

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Call stand alone functions
+        DisplayWelcome();
+        string name = PromptUserName();
+        int favNumber = PromptUserNumber();
+        double numberSqr = SquareNumber(favNumber);
+        DisplayResult(name, numberSqr);
+    }
+
+    // Display welcome message
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    // Ask user for their name
+    static string PromptUserName()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    // Ask user for their favorite number
+    static int PromptUserNumber()
+    {
+        Console.Write("Enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    // Square user's favorite number
+    static double SquareNumber(int number)
+    {
+        double square = Math.Pow(number, 2);
+        return square;
+
+        /* Could have written as:
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+        */
+    }
+
+    // Display message with user's name and number squared
+    static void DisplayResult(string name, double number)
+    {
+        Console.WriteLine($"{name}, your favorite number squared is {number}");
     }
 }
