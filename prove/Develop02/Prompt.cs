50313976bd29a8b63631414505e8352858706165
livrich <91569440+livrich@users.noreply.github.com>
2023-01-29 16:51:01 -0700

    Created prompt text file and class

---
+++ prove/Develop02/Prompt.cs
@@ -0,0 +1,34 @@
+using System;
+
+public class Prompt
+{
+    // There are no attributes for this class.
+
+    // Method to read designated file and return list of strings.
+    public List<string> ReadFile()
+    {
+        // Designated file where journal prompts are stored.
+        // Need to have full file path.
+        string file = @"C:\Users\olivi\OneDrive\Documents\Programming\CSE_210\cse210_csharp\prove\Develop02\prompts.txt";
+        // Read text file line by line into list.
+        List<string> lines = File.ReadAllLines(file).ToList();
+        return lines;
+    }
+
+    // Method to randomly generate and return a string prompt.
+    // Calls on ReadFile() method above to get list of prompts.
+    public string GeneratePrompt()
+    {
+        // Get list of prompts by reading file with prompts.
+        List<string> promptsList = ReadFile();
+        
+        // Get random number with Random class.
+        Random rnd = new Random();
+        // Min value is included, max value is not.
+        int number = rnd.Next(0, 5);
+        
+        // Get specified prompt by index value.
+        string prompt = promptsList[number];
+        return prompt;
+    }
+}
