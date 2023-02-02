a063e751b5c17684ae4eedefc6b6717a51b772ac
livrich <91569440+livrich@users.noreply.github.com>
2023-02-02 13:06:32 -0700

    Code and comments

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,12 +1,21 @@
 using System;
+using System.IO;
 
 public class Journal
 {
+    // Attribute for name of file.
     public string _filename;
-    public List<Entry> _allEntries;
 
-    public void AddEntry()
+    // Method to save allEntries list to text file.
+    public void SaveJournal(List<string> entries, string file)
     {
-        
+        File.WriteAllLines(file, entries);
     }
+
+    // Could not figure out how to get this to work.
+    // Instead included as part of Program.cs.
+    // public void LoadJournal(string file)
+    // {
+    //     File.ReadAllLines(file);
+    // }
 }
