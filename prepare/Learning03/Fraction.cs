647ea4c4bba8a42286ae7b7bbd31b96072df99da
livrich <91569440+livrich@users.noreply.github.com>
2023-02-04 13:33:01 -0700

    Code and comments, function class

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,45 @@
+using System;
+
+public class Fraction
+{
+    // Attributes
+    private int _top;
+    private int _bottom;
+
+    // Constructors
+    public Fraction()
+    {
+        // Default to 1/1
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // Methods
+    public string GetFractionString()
+    {
+        // Notice that this is not stored as a member variable.
+        // Is is just a temporary, local variable that will be 
+        // recomputed each time this is called.
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        // Notice that this is not stored as a member variable.
+        // Is will be recomputed each time this is called.
+        return (double)_top / (double)_bottom; 
+    }
+}
