0752d188a4249249d82fda8a02b213c3af818ed7
livrich <91569440+livrich@users.noreply.github.com>
2023-02-08 12:10:55 -0700

    Tried stuff

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        // This will start by displaying "AAA" and waiting for the user to press the enter key
+        Console.WriteLine("AAA");
+        Console.ReadLine();
+
+        // This will clear the console
+        Console.Clear();
+
+        // This will show "BBB" in the console where "AAA" used to be
+        Console.WriteLine("BBB");
     }
 }
