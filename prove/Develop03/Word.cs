f8c4f29147af967608a1f1c949323dea13da3daf
livrich <91569440+livrich@users.noreply.github.com>
2023-02-11 19:49:58 -0700

    tried to fix \n problem

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,24 +1,32 @@
 using System;
 
 class Word {
-    private List<string> _hiddenWords;
-    private List<string> _shownWords;
-    private string _text;
     private string _word;
 
-    public void Hide(){
-
+    // Constructor to set word.
+    public Word(string word)
+    {
+        _word = word;
     }
-
-    public void Show(){
-
-    }
-
-    public void IsHidden(){
-
-    }
-
-    public void GetText(){
-        
+    
+    // Method to replace characters of word with underscores.
+    public string ConvertToUnderscore()
+    {
+        // Don't hide line break '\n'
+        // Ya this if statement didn't do anything to
+        // fix the line break problem.
+        if (_word == "\n")
+        {
+            return "\n";
+        }
+        else
+        {
+            foreach (char character in _word)
+            {
+                string c = character.ToString();
+                _word = _word.Replace(c, "_");
+            }
+            return _word;
+        }
     }
 }
