e2d8df1fa1a0a34af4099d2f7e42aed52bdf07f9
livrich <91569440+livrich@users.noreply.github.com>
2023-02-18 16:55:59 -0700

    removed blank lines, added comments

--- prepare/Learning04/WritingAssignment.cs
+++ prepare/Learning04/WritingAssignment.cs
@@ -1,13 +1,13 @@
-
-
 public class WritingAssignment : Assignment {
     private string _title;
 
-    public WritingAssignment(string studentName, string topic, string title): 
-    base (studentName, topic) {
+    // Constructor: inherits studentName and topic from base class (Assignment)
+    public WritingAssignment(string studentName, string topic, string title)
+    : base (studentName, topic) {
         _title = title;
     }
 
+    // Can access _studentName because it is protected, not private
     public string GetWritingInformation() {
         return $"{_title} by {_studentName}";
     }
