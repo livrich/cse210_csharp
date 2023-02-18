e93517b85cc4cec65f6243128a4204ffffdb4c9d
livrich <91569440+livrich@users.noreply.github.com>
2023-02-18 16:56:03 -0700

    removed blank lines, added comments

--- prepare/Learning04/Assignment.cs
+++ prepare/Learning04/Assignment.cs
@@ -1,15 +1,14 @@
-
-
 public class Assignment {
+    // Instead of being protected, could leave private and make
+    // a getter method that returns _studentName so other classes
+    // could access this member variable.
     protected string _studentName;
     private string _topic;
-
 
     public Assignment(string studentName, string topic) {
         _studentName = studentName;
         _topic = topic;
     }
-
 
     public string GetSummary() {
         return $"{_studentName} - {_topic}";
