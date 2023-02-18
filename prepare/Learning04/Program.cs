d28523eaa4a369ebc2258355e17717de6e8c036d
livrich <91569440+livrich@users.noreply.github.com>
2023-02-18 16:56:09 -0700

    added comments

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,13 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Create and display base class
         Assignment assignment1 = new Assignment("Olivia", "Math");
         Console.WriteLine(assignment1.GetSummary());
 
+        // Create and display derived math class
         MathAssignment math1 = new MathAssignment("Jenny", "Calc", "4.2", "12-20");
         Console.WriteLine(math1.GetSummary());
         Console.WriteLine(math1.GetHomeworkList());
 
+        // Create and display derived writing class
         WritingAssignment write1 = new WritingAssignment("Lex", "English 101", "Thoughts on Book");
         Console.WriteLine(write1.GetSummary());
         Console.WriteLine(write1.GetWritingInformation());
