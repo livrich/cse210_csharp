22866e35d03bc9afe7d229459faba500443d96cc
livrich <91569440+livrich@users.noreply.github.com>
2023-02-18 16:37:30 -0700

    call MathAssignment class

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -6,5 +6,9 @@ class Program
     {
         Assignment assignment1 = new Assignment("Olivia", "Math");
         Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment math1 = new MathAssignment("Jenny", "Calc", "4.2", "12-20");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
     }
 }
