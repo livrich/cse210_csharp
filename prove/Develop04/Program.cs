01a102680dc9e334f203663daa678ff053c4c567
livrich <91569440+livrich@users.noreply.github.com>
2023-03-04 13:45:24 -0700

    menu, while loop, and if statement code

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        // bool run = true;
+        while (true)
+        {
+            DisplayMenu();
+            Console.Write("Select menu option: ");
+            string choice = Console.ReadLine();
+
+            if (choice == "1") {
+                BreathingActivity a1 = new BreathingActivity("Breathing");
+                a1.RunActivity();
+            } else if (choice == "2") {
+                Console.WriteLine("Reflect");
+            } else if (choice == "3") {
+                Console.WriteLine("List");
+            } else if (choice == "4") {
+                System.Environment.Exit(0);
+            } else {
+                Console.WriteLine("That was not a valid option.");
+            }
+        }
+        
+
+        void DisplayMenu()
+        {
+            Console.WriteLine("\nMenu:");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflecting Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Quit");
+        }
     }
 }
