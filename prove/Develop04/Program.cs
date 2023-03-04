57f3bd9b6a8cd0a8b1c272463e911d35ec64d41e
livrich <91569440+livrich@users.noreply.github.com>
2023-03-04 15:56:53 -0700

    added code to call ListingActivity class

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -11,13 +11,17 @@ class Program
             string choice = Console.ReadLine();
 
             if (choice == "1") {
-                BreathingActivity a1 = new BreathingActivity("Breathing");
-                a1.RunActivity();
+                BreathingActivity b1 = new BreathingActivity("Breathing");
+                b1.RunActivity();
+                Console.Clear();
             } else if (choice == "2") {
                 ReflectActivity r1 = new ReflectActivity("Reflecting");
                 r1.RunActivity();
+                Console.Clear();
             } else if (choice == "3") {
-                Console.WriteLine("List");
+                ListActivity l1 = new ListActivity("Listing");
+                l1.RunActivity();
+                Console.Clear();
             } else if (choice == "4") {
                 System.Environment.Exit(0);
             } else {
