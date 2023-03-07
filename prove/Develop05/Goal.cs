c808f47e9f446ec4af99687d2173b79dc85e4654
livrich <91569440+livrich@users.noreply.github.com>
2023-03-07 13:13:48 -0700

    outline class

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -2,5 +2,31 @@ using System;
 
 public abstract class Goal
 {
-    
+    // Attributes
+    protected string _name;
+    protected string _description;
+    protected int _points;
+    private bool _completed;
+
+    // Constructor
+    public Goal(string name, string description, int points)
+    {
+        _name = name;
+        _description = description;
+        _points = points;
+        _completed = false;
+    }
+
+    // Methods
+    public abstract bool RecordGoal();
+
+    public string CheckBox(List<bool> list, int index)
+    {
+        string checkBox = "[ ]";
+        if (list[index] is true)
+        {
+            checkBox = "[X]";
+        }
+        return checkBox;
+    }
 }
