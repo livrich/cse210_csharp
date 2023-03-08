36459d7ff20fb3569dcd9ca792fdca8cba1e705e
livrich <91569440+livrich@users.noreply.github.com>
2023-03-07 17:18:32 -0700

    code for creating object

--- prove/Develop05/ChecklistGoal.cs
+++ prove/Develop05/ChecklistGoal.cs
@@ -4,17 +4,43 @@ public class ChecklistGoal : Goal
 {
     // Attributes
     private int _bonusPoints;
+    private int _repsCompleted;
+    private int _totalRepetitions;
 
     // Constructor
     public ChecklistGoal(string name, string description, int points, 
-    int bonusPoints) : base(name, description, points)
+    int bonusPoints, int totalReps): base(name, description, points)
     {
         _bonusPoints = bonusPoints;
+        _repsCompleted = 0;
+        _totalRepetitions = totalReps;
     }
 
     // Methods
     public override bool RecordGoal()
     {
         return true;
-    }   
+    } 
+
+    public int GetRepsCompleted()
+    {
+        return _repsCompleted;
+    }
+
+    public int GetTotalReps()
+    {
+        return _totalRepetitions;
+    }
+
+    public void CheckCompleted()
+    {
+        if (_repsCompleted == _totalRepetitions)
+        {
+            _isCompleted = true;
+        }
+        else
+        {
+            _isCompleted = false;
+        }
+    }
 }
