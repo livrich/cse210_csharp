81a8b0dd5fae5267f3492fe063c909b3efca3780
livrich <91569440+livrich@users.noreply.github.com>
2023-04-01 17:47:20 -0600

    test product class

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -9,6 +9,10 @@ class Program
         // List of orders
         List<Order> orders = new List<Order>();
 
-        
+        // Instance of Product class
+        Product p = new Product("Rubber Duck", "123", 1.50, 20);
+        // Test to see if class is working
+        Console.WriteLine(p.GetName());
+        Console.WriteLine(p.ComputePrice());
     }
 }
