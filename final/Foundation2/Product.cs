d4cebc7b4e3890c3d2e7b963efbb3a8f4cf62019
livrich <91569440+livrich@users.noreply.github.com>
2023-04-01 17:47:12 -0600

    added methods

--- final/Foundation2/Product.cs
+++ final/Foundation2/Product.cs
@@ -4,8 +4,42 @@ public class Product
 {
     private string _name;
     private string _ID;
-    private float _price;
+    private double _price;
     private int _quantity;
 
-    
+    // Constructor
+    public Product(string name, string ID, double price, int quantity)
+    {
+        _name = name;
+        _ID = ID;
+        _price = price;
+        _quantity = quantity;
+    }
+
+    // Series of getter methods
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public string GetID()
+    {
+        return _ID;
+    }
+
+    public double GetPrice()
+    {
+        return _price;
+    }
+
+    public int GetQuantity()
+    {
+        return _quantity;
+    }
+
+    // Method to compute price based on quantity
+    public double ComputePrice()
+    {
+        return _price * _quantity;
+    }
 }
