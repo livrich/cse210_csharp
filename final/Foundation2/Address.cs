ab38e10e1aa10b3231df3a1367dba499b5647405
livrich <91569440+livrich@users.noreply.github.com>
2023-04-01 18:08:45 -0600

    added methods

--- final/Foundation2/Address.cs
+++ final/Foundation2/Address.cs
@@ -6,7 +6,56 @@ public class Address
     private string _city;
     private string _stateProvince;
     private string _country;
-    private bool _inUSA;
 
-    
+    // Constructor (does not initialize _inUSA attribute)
+    public Address(string street, string city, string stateProvince, string country)
+    {
+        _street = street;
+        _city = city;
+        _stateProvince = stateProvince;
+        _country = country;
+    }
+
+    // Series of getter methods
+    public string GetStreet()
+    {
+        return _street;
+    }
+
+    public string GetCity()
+    {
+        return _city;
+    }
+
+    public string GetStateProvince()
+    {
+        return _stateProvince;
+    }
+
+    public string GetCountry()
+    {
+        return _country;
+    }
+
+    // Method to determine if address is in USA
+    public bool IsInUSA()
+    {
+        if (_country == "US" ||
+        _country == "USA" ||
+        _country == "United States" ||
+        _country == "United States of America")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // Method to return address as one string
+    public string SingleString()
+    {
+        return ($"{_street}, {_city}, {_stateProvince}, {_country}");
+    }
 }
