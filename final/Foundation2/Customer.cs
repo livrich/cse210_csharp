8e204f89defaa34896787c7585503588acb87da7
livrich <91569440+livrich@users.noreply.github.com>
2023-04-02 16:17:57 -0600

    added methods

--- final/Foundation2/Customer.cs
+++ final/Foundation2/Customer.cs
@@ -6,5 +6,46 @@ public class Customer
     private string _name;
     private Address _address;
 
-    
+    // Constructor
+    public Customer(string name)
+    {
+        _name = name;
+        _address = CreateAddress();
+        _inUSA = _address.IsInUSA();
+    }
+
+    // Series of getters
+    public bool GetInUSA()
+    {
+        return _inUSA;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public Address GetAddress()
+    {
+        return _address;
+    }
+
+    // Method to call address class
+    private Address CreateAddress()
+    {
+        // Get information about address from user
+        Console.WriteLine("Enter the following address inforamtion");
+        Console.Write("Street: ");
+        string street = Console.ReadLine();
+        Console.Write("City: ");
+        string city = Console.ReadLine();
+        Console.Write("State/Province: ");
+        string stateProvince = Console.ReadLine();
+        Console.Write("Country: ");
+        string country = Console.ReadLine();
+
+        Address a = new Address(street, city, stateProvince, country);
+        // Address a = new Address("123 Sweet Street", "Kumamoto", "Kyushu", "Japan");
+        return a;
+    }
 }
