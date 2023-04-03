35c4f1153cd66a086a3e588a1f78e241bac3bed8
livrich <91569440+livrich@users.noreply.github.com>
2023-04-03 14:43:20 -0600

    got rid of few lines of code

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -6,10 +6,7 @@ class Program
     {
         Console.WriteLine("Hello Foundation3 World!");
 
-        // Create and display base class
-        Event e = new Event("How to Cook", "Come learn how to cook", "April 24", "6:00 PM", "Salt Palace");
-        e.Summary();
-
         Lecture l = new Lecture("How to Code", "Learn how to code", "June 6", "4:45 PM", "My House", "Me", 20);
+        l.Summary();
     }
 }
