30f40b105ad2d9aba5684db8726d2565e6b00b86
livrich <91569440+livrich@users.noreply.github.com>
2023-04-03 17:03:10 -0600

    made lines more readably

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -5,7 +5,8 @@ class Program
     static void Main(string[] args)
     {   
         // Create instance of Lecture class
-        Lecture l = new Lecture("How to Code 101", "Come learn the basics of coding.", "June 6", "4:45 PM", "Talented Person", 200);
+        Lecture l = new Lecture("How to Code 101", "Come learn the basics of coding.", 
+        "June 6", "4:45 PM", "Talented Person", 200);
         // Display each message type for Lecture object
         Console.WriteLine();
         Console.WriteLine(l.GetStandard());
@@ -17,7 +18,8 @@ class Program
 
 
         // Create instance of Reception class
-        Reception r = new Reception("Fancy Wedding", "Celebrate the wedding of the year with us.", "8/18/23", "6-8 PM", "comeToMyWedding@Wmail.com");
+        Reception r = new Reception("Fancy Wedding", "Celebrate the wedding of the year with us.", 
+        "8/18/23", "6-8 PM", "comeToMyWedding@Wmail.com");
         // Display each message type for Reception object
         Console.WriteLine();
         Console.WriteLine(r.GetStandard());
@@ -28,7 +30,8 @@ class Program
         Console.WriteLine();
 
         // Create instance of Outdoor class
-        Outdoor o = new Outdoor("Splish Splash Slide", "We are having a water party!", "Wednesday, July 12", "all day, come anytime!", "Sunny with few clouds");
+        Outdoor o = new Outdoor("Splish Splash Slide", "We are having a water party!", 
+        "Wednesday, July 12", "all day, come anytime!", "Sunny with few clouds");
         // Display each message type for Outdoor object
         Console.WriteLine();
         Console.WriteLine(o.GetStandard());
@@ -37,8 +40,5 @@ class Program
         Console.WriteLine();
         Console.WriteLine(o.GetShort());
         Console.WriteLine();
-        
-
-      
     }
 }
