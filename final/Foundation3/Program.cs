10bce6123624b23a633e0c383f0234784dc7c4c7
livrich <91569440+livrich@users.noreply.github.com>
2023-04-03 14:14:17 -0600

    tested classes

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation3 World!");
+
+        // Create and display base class
+        Event e = new Event("How to Cook", "Come learn how to cook", "April 24", "6:00 PM", "Salt Palace");
+        e.Summary();
+
+        Lecture l = new Lecture("How to Code", "Learn how to code", "June 6", "4:45 PM", "My House", "Me", 20);
     }
 }
