30de15021cdad98b30741be5799537bb00468a31
livrich <91569440+livrich@users.noreply.github.com>
2023-04-03 13:02:54 -0600

    took out hello world line

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -17,8 +17,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
-
         // List of orders
         List<Order> orders = new List<Order>();
 
