891e3772091e0a49d20a88e56cb3dfea40d620bb
livrich <91569440+livrich@users.noreply.github.com>
2023-04-04 16:05:03 -0600

    tested run class

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -5,13 +5,18 @@ class Program
     static void Main(string[] args)
     {
         // Testing Swim/Activity Classes
-        Swim swim = new Swim(30, 40);
+        Swim swim = new Swim(30);
         Console.WriteLine(swim.GetSummary());
         Console.WriteLine();
 
         // Testing Bike Class
         Bike bike = new Bike(40);
         Console.WriteLine(bike.GetSummary());
+        Console.WriteLine();
+
+        // Testing Run Class
+        Run run = new Run(60);
+        Console.WriteLine(run.GetSummary());
         Console.WriteLine();
     }
 }
