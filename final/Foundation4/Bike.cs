ee8c07c893688abec02efb202a31b54fdf3ddb41
livrich <91569440+livrich@users.noreply.github.com>
2023-04-04 16:04:12 -0600

    removed methods

--- final/Foundation4/Bike.cs
+++ final/Foundation4/Bike.cs
@@ -20,21 +20,9 @@ public class Bike : Activity
         /* Other than tire rotations, I don't know how
         to calculate distance for biking. */
 
-        // Get distance gone from user
-        Console.Write("How far did you bike? ");
+        // Get distance from user (Makes more sense)
+        Console.Write("How far did you bike? Miles: ");
         double distance = Double.Parse(Console.ReadLine());
         return distance;
-    }
-
-    // Override speed method
-    public override double CalcSpeed()
-    {
-        return (_distance / _duration) * 60;
-    }
-
-    // Override pace method
-    public override double CalcPace()
-    {
-        return _duration / _distance;
     }
 }
