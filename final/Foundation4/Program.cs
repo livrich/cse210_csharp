5053195868f78dbe204ad32149dcabb3209576ef
livrich <91569440+livrich@users.noreply.github.com>
2023-04-04 15:51:03 -0600

    tested bike class

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -6,6 +6,12 @@ class Program
     {
         // Testing Swim/Activity Classes
         Swim swim = new Swim(30, 40);
-        Console.WriteLine($"{swim.GetSummary()}");
+        Console.WriteLine(swim.GetSummary());
+        Console.WriteLine();
+
+        // Testing Bike Class
+        Bike bike = new Bike(40);
+        Console.WriteLine(bike.GetSummary());
+        Console.WriteLine();
     }
 }
