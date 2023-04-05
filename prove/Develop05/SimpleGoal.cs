ddae89edb8c43400b5b850a4b8c575ce9c312bb0
livrich <91569440+livrich@users.noreply.github.com>
2023-04-05 16:53:06 -0600

    implemented new methods

--- prove/Develop05/SimpleGoal.cs
+++ prove/Develop05/SimpleGoal.cs
@@ -5,15 +5,25 @@ public class SimpleGoal : Goal
     // Attributes
 
     // Constructor
-    public SimpleGoal(string name, string description, int points) 
-    : base(name, description, points)
+    public SimpleGoal(string type, string name, string description, int points) 
+    : base(type, name, description, points)
     {
 
     }
 
     // Methods
-    public override bool RecordGoal()
+    public override bool SetComplete()
     {
-        return true;
+        return _isComplete = true;
+    }
+
+    public override string GetFileSummary()
+    {
+        return $"{_type}:{_name},{_description},{_points},{_isComplete},{_checkBox}";
+    }
+
+    public override string GetDisplaySummary()
+    {
+        return $"{_checkBox} {_name} ({_description})";
     }
 }
