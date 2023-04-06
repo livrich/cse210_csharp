f8b230910858361653005666723775c55166c14f
livrich <91569440+livrich@users.noreply.github.com>
2023-04-05 20:53:31 -0600

    filled out class

--- prove/Develop05/ChecklistGoal.cs
+++ prove/Develop05/ChecklistGoal.cs
@@ -1,46 +1,57 @@
-// using System;
+using System;
 
-// public class ChecklistGoal : Goal
-// {
-//     // Attributes
-//     private int _bonusPoints;
-//     private int _repsCompleted;
-//     private int _totalRepetitions;
+public class ChecklistGoal : Goal
+{
+    // Attributes
+    private int _bonusPoints;
+    private int _repsCompleted;
+    private int _totalRepetitions;
 
-//     // Constructor
-//     public ChecklistGoal(string type, string name, string description, int points, 
-//     int bonusPoints, int totalReps, int repsCompleted = 0): base(type, name, description, points)
-//     {
-//         _bonusPoints = bonusPoints;
-//         _repsCompleted = repsCompleted;
-//         _totalRepetitions = totalReps;
-//     }
+    // Constructor
+    public ChecklistGoal(string type, string name, string description, int points, 
+    int bonusPoints, int totalReps, int repsCompleted = 0, bool isComplete = false, string checkBox = "[ ]"): 
+    base(type, name, description, points, isComplete, checkBox)
+    {
+        _bonusPoints = bonusPoints;
+        _repsCompleted = repsCompleted;
+        _totalRepetitions = totalReps;
+    }
 
-//     // Methods
-//     public override bool RecordGoal()
-//     {
-//         return true;
-//     } 
+    // Override method to set state of completed as true
+    // if repsCompleted = totalReps. (Else remains false)
+    public override bool SetComplete()
+    {
+        if (_repsCompleted == _totalRepetitions)
+        {
+            return _isComplete = true;
+        }
+        else
+        {
+            return _isComplete;
+        }
+    }
 
-//     public int GetRepsCompleted()
-//     {
-//         return _repsCompleted;
-//     }
+    // Override method to return summary for file saving
+    public override string GetFileSummary()
+    {
+        return $"{_type}:{_name},{_description},{_points},{_bonusPoints},{_isComplete},{_checkBox},{_repsCompleted},{_totalRepetitions}";
+    }
 
-//     public int GetTotalReps()
-//     {
-//         return _totalRepetitions;
-//     }
+    // Override method to return summary to display to screen
+    public override string GetDisplaySummary()
+    {
+        return $"{_checkBox} {_name} ({_description}) -- Currently completed {_repsCompleted}/{_totalRepetitions}";
+    } 
 
-//     public void CheckCompleted()
-//     {
-//         if (_repsCompleted == _totalRepetitions)
-//         {
-//             _isCompleted = true;
-//         }
-//         else
-//         {
-//             _isCompleted = false;
-//         }
-//     }
-// }
+    // Method to count how many times goal has been completed
+    public override int IncreaseRepetitions()
+    {
+        return _repsCompleted += 1;
+    }
+
+    // Method to get number of times goal has been completed
+    public override int GetRepetitions()
+    {
+        return _repsCompleted;
+    }
+}
